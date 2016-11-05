c7553662a71b87502524525093b661d46e656485
Loyd Cantos <LoydCantos@users.noreply.github.com>
2016-11-05 20:00:20 +0800

    Add files via upload

---
+++ Lexicographicgenerator/build/classes/lexicographicgenerator/Lexicographicgenerator.rs
@@ -0,0 +1,1 @@
+lexicographicgenerator.LexicographicGenerator
---
+++ Lexicographicgenerator/src/lexicographicgenerator/Lexicographicgenerator.java
@@ -0,0 +1,10 @@
+package lexicographicgenerator;
+public class Lexicographicgenerator {
+    public static void main(String[] args) {
+        String charset = "ABC";
+        for(int i = 0; i<charset.length(); i++){
+            System.out.println(charset.charAt(i));
+        }
+    }
+    
+}
